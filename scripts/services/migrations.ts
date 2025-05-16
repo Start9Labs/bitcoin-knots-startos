920bf903d782e246af0cf8e07283f471ae0baa0a
Léo Haf <leo@haf.ovh>
2025-05-16 12:44:41 +0200

    add migration and bump version to 29.0 for the next release

--- scripts/services/migrations.ts
+++ scripts/services/migrations.ts
@@ -71,6 +71,43 @@ export const migration: T.ExpectedExports.migration =
           type: "down",
         }),
       },
+      "29.0": {
+        up: compat.migrations.updateConfig(
+          (config: any) => {
+            config.coinstatsindex = false;
+            config.blkconstr.mempoolreplacement = {mode: "optout",};
+            config.softwareexpiry = 1825593420;
+            if (
+              !matches
+                .shape({
+                  advanced: matches.shape({
+                    blocknotify: matches.any,
+                  }),
+                })
+                .test(config)
+            ) {
+              throw new Error("Upgrading from Core to Knots is prohibited.");
+            } else {
+              return config;
+            }
+          },
+          true,
+          {
+            version: "29.0",
+            type: "up",
+          }
+        ),
+        down: compat.migrations.updateConfig((config: any) => {
+          delete config.softwareexpiry;
+
+          return config;
+        },
+        true,
+        {
+          version: "29.0",
+          type: "down",
+        }),
+      }
     },
-    "28.1.0.2"
+    "29.0.0"
   );
