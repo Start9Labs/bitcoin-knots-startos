94bd13922831f22efb2fb05c2095bd147dd9391e
Shadowy Super Coder <musashidisciple@proton.me>
2025-06-25 13:31:14 -0600

    add migration from core

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,2 +1,4 @@
+import { current as bitcoinCoreCurrent } from 'bitcoind-startos/startos/install/versions'
+
 export { v28_1_0_3 as current } from './v28_1_0_3'
-export const other = []
+export const other = [bitcoinCoreCurrent]
--- startos/install/versions/v28_1_0_3.ts
+++ startos/install/versions/v28_1_0_3.ts
@@ -1,15 +1,16 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
-import { bitcoinConfFile, shape } from '../../fileModels/bitcoin.conf'
-import { load } from 'js-yaml'
-import { readFile } from 'fs/promises'
-import { bitcoinConfDefaults } from '../../utils'
 import { storeJson } from '../../fileModels/store.json'
+import { current as bitcoinCoreCurrent } from 'bitcoind-startos/startos/install/versions'
 
 export const v28_1_0_3 = VersionInfo.of({
   version: '#knots:28.1:3-alpha.0',
   releaseNotes: 'Revamped for StartOS 0.4.0',
   migrations: {
-    // @TODO knots specific migrations - should be able to just use the file helper to read the existing bitcoin.conf
+    other: {
+      [bitcoinCoreCurrent.options.version]: async ({ effects }) => {
+        // nothing should be required here as Knots should be able to take Bitcoin Core's bitcoin.conf as it exists without any changes for it to be used by knots as-is
+      }
+    },
     up: async ({ effects }) => {
       await storeJson.write(effects, {
         reindexBlockchain: false,
@@ -17,150 +18,8 @@ export const v28_1_0_3 = VersionInfo.of({
         fullySynced: false,
         snapshotInUse: false,
       })
-      try {
-        const configYaml = load(
-          await readFile(
-            '/media/startos/volumes/main/start9/config.yaml',
-            'utf8',
-          ),
-        ) as {
-          ['peer-tor-address']: string
-          rpc: {
-            advanced: {
-              auth: string[]
-              servertimeout: number
-              threads: number
-              workqueue: number
-            }
-          }
-          'zmq-enabled': boolean
-          txindex: boolean
-          coinstatsindex: boolean
-          wallet: {
-            enable: boolean
-            avoidpartialspends: boolean
-            discardfee: number
-          }
-          advanced: {
-            mempool: {
-              persistmempool: boolean
-              maxmempool: number
-              mempoolexpiry: number
-              mempoolfullrbf: boolean
-              permitbaremultisig: boolean
-              datacarrier: boolean
-              datacarriersize: number
-            }
-            peers: {
-              listen: boolean
-              onlyconnect: boolean
-              onlyonion: boolean
-              v2transport: boolean
-              addnode: string[]
-            }
-            pruning: { mode: 'disabled' } | { mode: 'automatic'; size: number }
-            dbcache: number | null
-            blockfilters: {
-              blockfilterindex: boolean
-              peerblockfilters: boolean
-            }
-            bloomfilters: { peerbloomfilters: boolean }
-          }
-        }
-
-        const {
-          ['peer-tor-address']: peerTorAddress,
-          rpc: {
-            advanced: { auth, servertimeout, threads, workqueue },
-          },
-          ['zmq-enabled']: zmq,
-          txindex,
-          coinstatsindex,
-          wallet: { enable, avoidpartialspends, discardfee },
-          advanced: {
-            mempool: {
-              persistmempool,
-              maxmempool,
-              mempoolexpiry,
-              mempoolfullrbf,
-              permitbaremultisig,
-              datacarrier,
-              datacarriersize,
-            },
-            peers: { listen, onlyconnect, onlyonion, v2transport, addnode },
-            pruning,
-            dbcache,
-            blockfilters: { blockfilterindex, peerblockfilters },
-            bloomfilters: { peerbloomfilters },
-          },
-        } = configYaml
-
-        const pruned = pruning.mode === 'automatic'
-
-        const structuredConf: typeof shape._TYPE = {
-          // RPC
-          rpcbind: pruned ? '127.0.0.1:18332' : '0.0.0.0:8332',
-          rpcallowip: pruned ? '127.0.0.1/32' : '0.0.0.0/0',
-          rpcauth: auth,
-          rpcservertimeout: servertimeout,
-          rpcthreads: threads,
-          rpcworkqueue: workqueue,
-          rpccookiefile: bitcoinConfDefaults.rpccookiefile,
-          whitelist: ['172.18.0.0/16'],
-
-          // Mempool
-          persistmempool: persistmempool,
-          maxmempool,
-          mempoolexpiry,
-          mempoolfullrbf: mempoolfullrbf,
-          permitbaremultisig: permitbaremultisig,
-          datacarrier: datacarrier,
-          datacarriersize,
-
-          // Peers
-          listen: listen,
-          onlynet: onlyonion ? ['onion'] : undefined,
-          v2transport: v2transport,
-
-          // Wallet
-          disablewallet: !enable,
-          avoidpartialspends: avoidpartialspends,
-          discardfee,
-
-          // Other
-          externalip: peerTorAddress,
-          coinstatsindex: coinstatsindex,
-          txindex: txindex,
-          dbcache: dbcache || bitcoinConfDefaults.dbcache,
-          peerbloomfilters: peerbloomfilters,
-          blockfilterindex: blockfilterindex ? 'basic' : undefined,
-          peerblockfilters: peerblockfilters,
-          prune: pruned ? pruning.size : bitcoinConfDefaults.prune,
-          bind: listen ? '0.0.0.0:8333' : bitcoinConfDefaults.bind,
-
-          // ZMQ
-          zmqpubrawblock: zmq ? 'tcp://0.0.0.0:28332' : undefined,
-          zmqpubhashblock: zmq ? 'tcp://0.0.0.0:28332' : undefined,
-          zmqpubrawtx: zmq ? 'tcp://0.0.0.0:28333' : undefined,
-          zmqpubhashtx: zmq ? 'tcp://0.0.0.0:28333' : undefined,
-          zmqpubsequence: zmq ? 'tcp://0.0.0.0:28333' : undefined,
-        }
-
-        if (onlyconnect) {
-          Object.assign({ structuredConf, connect: addnode })
-        } else {
-          Object.assign({ structuredConf, addnode: addnode })
-        }
-
-        await bitcoinConfFile.merge(effects, structuredConf)
-      } catch {
-        await bitcoinConfFile.write(effects, {
-          ...bitcoinConfDefaults,
-          externalip: 'initial-setup',
-        })
-      }
     },
     down: IMPOSSIBLE,
   },
-}).satisfies('28.1:3-alpha.3')
+}).satisfies(bitcoinCoreCurrent.options.version)
 
