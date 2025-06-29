7b6fbffb35e33d7fa0d13c99ed0ca7e954e86757
Shadowy Super Coder <musashidisciple@proton.me>
2025-06-29 12:06:48 -0600

    rename current to knotsCurrent

--- startos/install/versionGraph.ts
+++ startos/install/versionGraph.ts
@@ -1,11 +1,11 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { current, other } from './versions'
+import { knotsCurrent, other } from './versions'
 import { storeJson } from '../fileModels/store.json'
 import { bitcoinConfFile } from '../fileModels/bitcoin.conf'
 import { bitcoinConfDefaults } from '../utils'
 
 export const versionGraph = VersionGraph.of({
-  current,
+  current: knotsCurrent,
   other,
   preInstall: async (effects) => {
     await storeJson.write(effects, {
--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,4 +1,4 @@
 import { current as bitcoinCoreCurrent } from 'bitcoind-startos/startos/install/versions'
 
-export { v28_1_0_3 as current } from './v28_1_0_3'
+export { v28_1_0_3 as knotsCurrent } from './v28_1_0_3'
 export const other = [bitcoinCoreCurrent]
