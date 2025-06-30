2b3d7372e1b1c93f6cb133925bf952052dfd4e22
Shadowy Super Coder <musashidisciple@proton.me>
2025-06-30 15:06:40 -0600

    beta.32

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,4 +1,4 @@
-import { current as bitcoinCoreCurrent } from 'bitcoind-startos/startos/install/versions'
+import { coreCurrent } from 'bitcoind-startos/startos/install/versions'
 
 export { v28_1_0_3 as knotsCurrent } from './v28_1_0_3'
-export const other = [bitcoinCoreCurrent]
+export const other = [coreCurrent]
--- startos/install/versions/v28_1_0_3.ts
+++ startos/install/versions/v28_1_0_3.ts
@@ -1,34 +1,61 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 import { storeJson } from '../../fileModels/store.json'
-import { current as bitcoinCoreCurrent } from 'bitcoind-startos/startos/install/versions'
+import { coreCurrent as coreV28_1_3 } from 'bitcoind-startos/startos/install/versions'
 import { bitcoinConfFile } from '../../fileModels/bitcoin.conf'
 import { bitcoinConfDefaults } from '../../utils'
+import { bitcoinConfDefaults as coreDefaults } from 'bitcoind-startos/startos/utils'
 
 export const v28_1_0_3 = VersionInfo.of({
-  version: '#knots:28.1:3-alpha.0',
+  version: '#knots:28.1:3-alpha.1',
   releaseNotes: 'Revamped for StartOS 0.4.0',
   migrations: {
     other: {
-      [bitcoinCoreCurrent.options.version]: async ({ effects }) => {
-        /*
-          We want to merge bitcoin knots defaults into bitcoin.conf if those options
-          were not present in bitcoin.conf at flavor migration
-        */
-        const existingBitcoinConf = await bitcoinConfFile.read().once()
-        const nonConstDefaults: Record<string, any> = { ...bitcoinConfDefaults }
 
-        if (existingBitcoinConf) {
-          const newOptions: Record<string, any> = {}
-          for (const k in nonConstDefaults) {
-            if (!(k in existingBitcoinConf)) {
-              newOptions[k] = nonConstDefaults[k]
+      [coreV28_1_3.options.version]: {
+        // Core -> Knots
+        up: async ({ effects }) => {
+          /*
+            We want to merge bitcoin knots defaults into bitcoin.conf if those options
+            were not present in bitcoin.conf at flavor migration
+          */
+          const existingBitcoinConf = await bitcoinConfFile.read().once()
+          const nonConstDefaults: Record<string, any> = { ...bitcoinConfDefaults }
+  
+          if (existingBitcoinConf) {
+            const newOptions: Record<string, any> = {}
+            for (const k in nonConstDefaults) {
+              if (!(k in existingBitcoinConf)) {
+                newOptions[k] = nonConstDefaults[k]
+              }
             }
+            await bitcoinConfFile.merge(effects, newOptions)
+          } else {
+            // Write the bitcoin.conf if it doesn't exist
+            await bitcoinConfFile.write(effects, bitcoinConfDefaults)
           }
-          await bitcoinConfFile.merge(effects, newOptions)
-        } else {
-          // Write the bitcoin.conf if it doesn't exist
-          await bitcoinConfFile.write(effects, bitcoinConfDefaults)
-        }
+        },
+        // Knots -> Core
+        down: async ({ effects }) => {
+          /*
+            We want to merge bitcoin core defaults into bitcoin.conf if those options
+            were not present in bitcoin.conf at flavor migration
+          */
+          const existingBitcoinConf = await bitcoinConfFile.read().once()
+          const nonConstDefaults: Record<string, any> = { ...coreDefaults }
+  
+          if (existingBitcoinConf) {
+            const newOptions: Record<string, any> = {}
+            for (const k in nonConstDefaults) {
+              if (!(k in existingBitcoinConf)) {
+                newOptions[k] = nonConstDefaults[k]
+              }
+            }
+            await bitcoinConfFile.merge(effects, newOptions)
+          } else {
+            // Write the bitcoin.conf if it doesn't exist
+            await bitcoinConfFile.write(effects, coreDefaults)
+          }
+        },
       },
     },
     up: async ({ effects }) => {
@@ -41,4 +68,4 @@ export const v28_1_0_3 = VersionInfo.of({
     },
     down: IMPOSSIBLE,
   },
-}).satisfies(bitcoinCoreCurrent.options.version)
+}).satisfies(coreV28_1_3.options.version)
