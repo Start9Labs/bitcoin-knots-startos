dfe19d6d1fe4b32841a2ad24d762d98f839c4b55
Léo Haf <leo@haf.ovh>
2025-09-03 12:34:54 +0000

    Update to alpha5

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,5 +1,6 @@
 import { coreCurrent } from 'bitcoind-startos/startos/install/versions'
 import { v28_1_0_3 } from './v28_1_0_3'
+import { v29_1_0 } from './v29_1_0'
 
-export { v29_1_0 as knotsCurrent } from './v29_1_0'
-export const other = [coreCurrent, v28_1_0_3]
+export { v29_1_0_5 as knotsCurrent } from './v29_1_0_5'
+export const other = [coreCurrent, v28_1_0_3, v29_1_0]
---
+++ startos/install/versions/v29_1_0_5.ts
@@ -0,0 +1,11 @@
+import { VersionInfo } from '@start9labs/start-sdk'
+
+
+export const v29_1_0_5 = VersionInfo.of({
+  version: '#knots:29.1:0-alpha.5',
+  releaseNotes: 'Revamped for StartOS 0.4.0',
+  migrations: {
+    up: async () => {},
+    down: async () => {},
+  }
+})
