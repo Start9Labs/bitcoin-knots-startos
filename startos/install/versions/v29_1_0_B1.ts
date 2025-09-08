1c540ad9d9e3172d336c04ec1c7938d0453e3667
Léo Haf <leo@haf.ovh>
2025-09-08 14:47:18 +0000

    update to beta 1

---
+++ startos/install/versions/v29_1_0_B1.ts
@@ -0,0 +1,11 @@
+import { VersionInfo } from '@start9labs/start-sdk'
+
+
+export const v29_1_0_B1 = VersionInfo.of({
+  version: '#knots:29.1:1-beta.1',
+  releaseNotes: 'Revamped for StartOS 0.4.0',
+  migrations: {
+    up: async () => {},
+    down: async () => {},
+  }
+})
