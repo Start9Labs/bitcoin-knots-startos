32b676887177d3880128132b69fe2d374ff8e1a3
Léo Haf <leo@haf.ovh>
2025-09-24 12:37:44 +0000

    consolidate migration and update sdk
    Signed-off-by: Léo Haf <leo@haf.ovh>

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,7 +1,3 @@
-import { coreCurrent } from 'bitcoind-startos/startos/install/versions'
-import { v28_1_0_3 } from './v28_1_0_3'
-import { v29_1_0 } from './v29_1_0'
-import { v29_1_0_5 } from './v29_1_0_5'
-
 export { v29_1_0_B1 as knotsCurrent } from './v29_1_0_B1'
-export const other = [coreCurrent, v28_1_0_3, v29_1_0, v29_1_0_5]
+
+export const other = []
