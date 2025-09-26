e9876c538e153e9ec26a3133dccb653aecd36075
Léo Haf <leo@haf.ovh>
2025-09-26 13:45:40 +0000

    bump to beta3

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,3 +1,4 @@
-export { v29_1_0_B1 as knotsCurrent } from './v29_1_0_B1'
+export { v29_1_0_B3 as knotsCurrent } from './v29_1_0_B3'
+import { v29_1_0_B1 } from './v29_1_0_B1'
 
-export const other = []
+export const other = [v29_1_0_B1]
---
+++ startos/install/versions/v29_1_0_B3.ts
@@ -0,0 +1,10 @@
+import { VersionInfo } from '@start9labs/start-sdk'
+
+export const v29_1_0_B3 = VersionInfo.of({
+  version: '#knots:29.1:3-beta.3',
+  releaseNotes: 'Revamped for StartOS 0.4.0',
+  migrations: {
+    up: async () => {},
+    down: async () => {},
+  }
+})
