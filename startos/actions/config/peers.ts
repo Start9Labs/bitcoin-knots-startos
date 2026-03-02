73517100a0d42865e5606ef3ccf591b85b46150f
Matt Hill <mattnine@protonmail.com>
2026-03-01 23:41:58 -0700

    fix bug

--- startos/actions/config/peers.ts
+++ startos/actions/config/peers.ts
@@ -106,7 +106,7 @@ export const peerConfig = sdk.Action.withInput(
 
   // the execution function
   async ({ effects, input }) => {
-    const { i2psam, externalip } = input
+    const { i2psam, externalip, ...confInput } = input
 
     if (externalip === 'create-tor') {
       await storeJson.merge(effects, { wantsOnion: true })
@@ -124,7 +124,7 @@ export const peerConfig = sdk.Action.withInput(
             ? undefined
             : externalip,
       },
-      ...input,
+      ...confInput,
     })
   },
 )
