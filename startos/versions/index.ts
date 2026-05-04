2739e0b56f64cc154d69fdd2303ea368ffaa1834
Léo Haf <leo@haf.ovh>
2026-05-04 16:47:20 +0200

    Bump to v29.3-7

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,7 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { v29_3_6 } from './v29.3_6'
+import { v29_3_7 } from './v29.3_7'
 
 export const versionGraph = VersionGraph.of({
-  current: v29_3_6,
+  current: v29_3_7,
   other: [],
 })
